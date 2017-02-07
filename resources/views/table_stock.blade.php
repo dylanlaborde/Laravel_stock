b2dec9b7a19e6b01be61c73b2068c66d6b4ec075
Dylan <dylan4047@gmail.com>
2017-02-07 12:21:56 +0100

    recuperation des info et affichage dans un tableau

--- resources/views/table_stock.blade.php
+++ resources/views/table_stock.blade.php
@@ -1,10 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.css">
 	<meta charset="UTF-8">
 	<title>acceuil</title>
 </head>
 <body>
-	<h1>liste des produit</h1>
+
+	<div class="ui grid">
+		<div class="six wide column"></div>
+		<h1>liste des produit</h1>
+	</div>
+	<div class="ui grid">
+		<div class="four wide column"></div>
+		<table class="ui striped table">
+			<thead>
+				
+				<tr>
+					<th>id</th>
+					<th>name</th>
+					<th>price</th>		
+					<th>descrition</th>		
+					<th>stock</th>		
+				</tr>		
+			</thead>
+			<tbody>
+				@foreach($produit as $key)
+				<tr>
+					<td>{{$key->id}}</td>
+					<td>{{$key->name}}</td>
+					<td>{{$key->price}}</td>
+					<td>{{$key->descrition}}</td>
+					<td>{{$key->stock}}</td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+
+	</div>
 </body>
 </html>
