b7e43f714567408ce9d229c77edc65bfba77fa68
Dylan <dylan4047@gmail.com>
2017-02-08 09:21:15 +0100

    ajout function affichage des detail + traitement stock +et-

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -11,4 +11,23 @@ class StockController extends Controller
 		$product=Stock::all();
 		return view('stocks.table_stock',['produit'=>$product]);
 	}
+	public function getDetail($id){
+		$detail=Stock::find($id);
+		return view('stocks.show',['details'=>$detail]);
+	}
+	public function postSell($id){
+
+		$detail=Stock::find($id);
+		$detail->stock--;
+		$detail->save();
+		return back();
+	}
+	public function postRefill($id){
+		
+		$detail=Stock::find($id);
+		$detail->stock++;
+		$detail->save();
+		return back();
+
+	}
 }
